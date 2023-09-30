09aaabe020f33b3fe583cc17980ef809c8dd2aef
ray-bao-mcgill <ray.bao@mail.mcgill.ca>
2023-09-30 16:31:02 -0400

    navbar

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -5,11 +5,7 @@ import { ServiceAPI } from "../../infrastructure";
 import "./Home.style.scss";
 
 function Home() {
-<<<<<<< HEAD
   const [products, setProducts] = useState([]);
-=======
-  const [products, setProducts] = useState([ {title:"hello", description:"testetstetestetstest", price:23.5,url:"https://www.cs.rochester.edu/~pawlicki/ECOMM/tem/HTML/Figures%20Ch1to4/figure4-6.jpg"}, {title:"hello", description:"testetstetestetstest", price:23.5,url:"https://www.cs.rochester.edu/~pawlicki/ECOMM/tem/HTML/Figures%20Ch1to4/figure4-6.jpg"}]);
->>>>>>> 870cad4c97baf892c4a18e665a4fcd687252410b
 
   useEffect(() => {
     const fetchData = async () => {
@@ -22,7 +18,7 @@ function Home() {
 
   return (
     <Page>
-      <div className="parallax">
+      <div className="parallax"></div>
         <div className="home-page">
           <h1 className="home-page__title"><br /><br />Home</h1>
           <h2>Products:</h2>
@@ -40,7 +36,6 @@ function Home() {
               </Link>
             ))}
           </div>
-        </div>
         </div>
     </Page>
   );
--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -14,9 +14,7 @@ function Login() {
     try {
       console.log("hello")
       const message = await login("admin@email.com", "password");
-      setMessage(message)
     } catch (error) {
-      setMessage(message)
     }
   };
 
@@ -29,18 +27,12 @@ function Login() {
   return (
     <Page>
       <div className="login-page">
-<<<<<<< HEAD
         <h1><br /><br />Login</h1>
-        <button onClick={() => attemptLogin()}>
-          Login (as user set in code)
-=======
-        <h1>Login</h1>
         <form action="" method="post">
         <p>ID: <input name="email" className="username" type="text"/></p>
         <p>PW: <input name="password" className="password" type="text"/></p>
         <button onClick={(e) => attemptLogin(e)}>
           Login
->>>>>>> 870cad4c97baf892c4a18e665a4fcd687252410b
         </button>
         {message && <p>{message}</p>}
         </form>
