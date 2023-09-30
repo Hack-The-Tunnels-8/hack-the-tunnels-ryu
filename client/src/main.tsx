09a63b2aaf7582b0895484934748bfe2aa748437
My Name <raymondliusmailbox@gmail.com>
2023-09-30 13:43:51 -0400

    add 404 page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -12,6 +12,7 @@ import {
   SignUp,
 } from "./pages";
 import { AccountProvider } from "./context";
+import  NotFound  from "./pages/NotFound/404";
 import "./index.scss";
 
 const router = createBrowserRouter([
@@ -36,7 +37,7 @@ const router = createBrowserRouter([
     element: <Product />,
   },
   {
-    path: "/sign-up",
+    path: "/sign-upp",
     element: <SignUp />,
   },
   {
@@ -45,8 +46,8 @@ const router = createBrowserRouter([
   },
   {
     path: "*",
-    element: <div>404</div>,
-  },
+    element: <NotFound />,
+  }
 ]);
 
 export function Root() {
